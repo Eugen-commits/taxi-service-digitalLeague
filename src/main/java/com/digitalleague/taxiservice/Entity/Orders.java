e67540a4a54c258c581c988b1326ef211b03fb6f
Eugene Zykin <porridge52@gmail.com>
2021-09-22 21:27:17 +0300

    add field

--- src/main/java/com/digitalleague/taxiservice/Entity/Orders.java
+++ src/main/java/com/digitalleague/taxiservice/Entity/Orders.java
@@ -1,4 +1,17 @@
 package com.digitalleague.taxiservice.Entity;
 
 public class Orders {
+    private String name;
+
+    public Orders(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
